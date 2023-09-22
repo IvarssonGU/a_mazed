c0f30ca108f281d9a06519de3b35ee321b346315
Ivarsson <gusivarol@student.gu.se>
2023-09-22 14:22:40 +0200

    noaccountcasino

--- amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
+++ amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
@@ -3,10 +3,7 @@ package amazed.solver;
 import amazed.maze.Maze;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.concurrent.ConcurrentSkipListSet;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -21,12 +18,11 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ForkJoinSolver extends SequentialSolver {
 
-    private final LinkedList<ForkJoinSolver> forkedSolvers = new LinkedList<>();
+    private final ConcurrentLinkedQueue<ForkJoinSolver> forkedSolvers = new ConcurrentLinkedQueue<>();
     private static final Set<Integer> visited = new ConcurrentSkipListSet<>(); // Create a thread-safe set
     private final Stack<Integer> frontier = new Stack<>();
-    //private static final Map<Integer, Integer> predecessor; //Create a thread-safe Map
     private static final AtomicBoolean goalFound = new AtomicBoolean();
-    private ArrayList<Integer> result = new ArrayList<>();
+    private CopyOnWriteArrayList<Integer> result = new CopyOnWriteArrayList<>();
 
     /**
      * Creates a solver that searches in <code>maze</code> from the
@@ -37,7 +33,6 @@ public class ForkJoinSolver extends SequentialSolver {
     public ForkJoinSolver(Maze maze, Integer next)
     {
         this(maze);
-        //this.predecessor = new ConcurrentSkipListMap<>();
         start = next;
     }
 
@@ -60,7 +55,6 @@ public class ForkJoinSolver extends SequentialSolver {
 
     public ForkJoinSolver(Maze maze) {
         super(maze);
-
     }
 
     /**
@@ -87,14 +81,9 @@ public class ForkJoinSolver extends SequentialSolver {
 
             if (maze.hasGoal(current)) {
                 maze.move(player, current); //Move the player to the goal
-                visited.add(current);
                 goalFound.set(true);
-                for (Integer node : pathFromTo(start, current)){
-                    if (node != null) {
-                        result.add(node);
-                    }
-                }
-                //result.addAll(pathFromTo(start, current));
+                result.addAll(pathFromTo(start, current));
+                System.out.println(predecessor);
                 System.out.println("goal child " + result);
                 return result; // search finished: reconstruct and return path
             }
@@ -119,21 +108,12 @@ public class ForkJoinSolver extends SequentialSolver {
                         popAndAdd(); //same logic as above
                         popAndAdd();
                 }
-
             }
-
-            // testa att tvinga alla att stänga av vid goalFound
-
-            // to fork a new thread you just create a new instance of ForkJoinSolver,
-            // with suitable parameters, and call fork() on the instance.
-
-        } //while loop end
+        } while (!frontier.isEmpty() && !goalFound.get()); //while loop end
 
         for (ForkJoinSolver task : forkedSolvers) {
-            if (task.join() != null) {
-                System.out.println(result);
+            if (task.join() != null && !task.result.isEmpty()) {
                 result.addAll(task.result);
-                result.addAll(pathFromTo(start, current));
             }
         }
 
@@ -148,7 +128,7 @@ public class ForkJoinSolver extends SequentialSolver {
      * Pops an int from the frontier stack, creates a new ForkJoinSolver with all the necessary
      * parameters and adds this ForkJoinSolver to the list with tasks to be processed
      */
-    synchronized private void popAndAdd() {
+        private void popAndAdd() {
         if (!frontier.isEmpty()) {
             Integer next = frontier.pop();
             if (!visited.contains(next)) {
